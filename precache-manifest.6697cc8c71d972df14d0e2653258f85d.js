2176bc56476455fb32f3c7d379d57642c4020fe6
Andrew Lee <yl3794@columbia.edu>
2019-05-20 19:44:05 -0500

    Updates

---
+++ precache-manifest.6697cc8c71d972df14d0e2653258f85d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc606d11fa44ad352ae9dd3f4a211c1",
+    "url": "/utatp/index.html"
+  },
+  {
+    "revision": "67823edd67ef44696c64",
+    "url": "/utatp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/utatp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "67823edd67ef44696c64",
+    "url": "/utatp/static/js/main.f9f14ec0.chunk.js"
+  },
+  {
+    "revision": "2d023225d72ee4b73223",
+    "url": "/utatp/static/js/runtime~main.7a5a72a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/utatp/static/media/logo.5d5d9eef.svg"
+  }
+]);
